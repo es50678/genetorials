00e13a53b2206b9b8e68ccb35dd62ceaee6c2d55
Eduardo Hernandez Soto <es50678@gmail.com>
2021-03-15 16:16:15 -0400

    scaffold cpp

--- cpp/interview-prep/two-sum.cpp
+++ cpp/interview-prep/two-sum.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int> nums, int target) {
+        return nums;
+    }
+};
 
 int main() {
-    std::cout << "hello world";
+    Solution test = *new Solution();
+
+    vector<int> input { 5, 4, 3, 2, 3, 4};
+
+    vector<int> res = test.twoSum(input, 5);
+
     return 0;
 }
