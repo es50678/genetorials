d1e9146fd0a6ef1357e126bbc99cc1d0b412ed8e
Eduardo Hernandez Soto <es50678@gmail.com>
2021-03-17 18:58:11 -0400

    add rust

---
+++ rust/interview-prep/two-sum/target/debug/deps/two_sum-96181d0d19b433cc.d
@@ -0,0 +1,5 @@
+/Users/eduardosoto/Documents/personal/training/brilliant-jupyter/rust/interview-prep/two-sum/target/debug/deps/two_sum-96181d0d19b433cc.rmeta: src/main.rs
+
+/Users/eduardosoto/Documents/personal/training/brilliant-jupyter/rust/interview-prep/two-sum/target/debug/deps/two_sum-96181d0d19b433cc.d: src/main.rs
+
+src/main.rs:
---
+++ rust/interview-prep/two-sum/target/debug/deps/two_sum-e230c8e1321746e1.d
@@ -0,0 +1,5 @@
+/Users/eduardosoto/Documents/personal/training/brilliant-jupyter/rust/interview-prep/two-sum/target/debug/deps/two_sum-e230c8e1321746e1.rmeta: src/main.rs
+
+/Users/eduardosoto/Documents/personal/training/brilliant-jupyter/rust/interview-prep/two-sum/target/debug/deps/two_sum-e230c8e1321746e1.d: src/main.rs
+
+src/main.rs:
